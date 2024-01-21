4d571b91cf993f77c7cc760c7df31a0eb74da15f
Alexxxxand <sanya072011@gmail.com>
2024-01-21 21:25:56 +0200

    first commit

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget -P thorium/ https://github.com/Alex313031/thorium/releases/download/M119.0.6045.214/Thorium_Browser_119.0.6045.214.AppImage
+sudo cp thorium/ /usr/local/
+sudo cp ~/.local/share/applications
