33fecb9a4280ab87d0806c2d0f6c3361fb3ca46f
Alex <sanya072011@gmail.com>
2024-03-03 13:06:44 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-wget -P thorium/ https://github.com/Alex313031/thorium/releases/download/M119.0.60>
+wget -P thorium/ https://github.com/Alex313031/thorium/releases/download/M121.0.6167.204/Thorium_Browser_121.0.6167.204_AVX.AppImage
 sudo cp -r thorium/ /usr/local/
 sudo cp thorium.desktop ~/.local/share/applications
-sudo chmod +x /usr/local/thorium/Thorium_Browser_119.0.6045.214.AppImage
+sudo chmod +x /usr/local/thorium/Thorium_Browser_121.0.6167.204_AVX.AppImage
 sudo chown $USER:$USER -R /usr/local/thorium
