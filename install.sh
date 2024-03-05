16c2237cae88bafb86af201eb1d7d25062c5c47c
arsenii754 <arseniipilipchuk@gmail.com>
2024-03-05 08:16:11 +0000

    Refactor code, Add AVX2/AVX comp. check

--- install.sh
+++ install.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
-wget -P thorium/ https://github.com/Alex313031/thorium/releases/download/M121.0.6167.204/Thorium_Browser_121.0.6167.204_AVX.AppImage
-sudo cp -r thorium/ /usr/local/
-sudo cp thorium.desktop ~/.local/share/applications
-sudo chmod +x /usr/local/thorium/Thorium_Browser_121.0.6167.204_AVX.AppImage
-sudo chown $USER:$USER -R /usr/local/thorium
+
+links=$(curl -s "https://api.github.com/repos/Alex313031/thorium/releases/latest" | grep -o -E "https://.*\.AppImage")
+
+if [[ $(cat /proc/cpuinfo | grep -o -E 'avx2' | head -1) ]]; then
+    link=$(echo "$links" | grep "AVX2" | head -1)
+elif [[ $(cat /proc/cpuinfo | grep -o -E 'avx' | head -1) ]]; then
+    link=$(echo "$links" | grep "AVX" | head -1)
+else
+    link=$(echo "$links" | grep "SSE3" | head -1)
+fi
+
+curl -L -o "Thorium_Browser.AppImage" "$link"
+chmod +x "Thorium_Browser.AppImage"
+
+sudo mkdir -p "/usr/local/thorium/"
+sudo cp "Thorium_Browser.AppImage" "/usr/local/thorium/" ; sudo cp "thorium_logo.png" "/usr/local/thorium"
+sudo chown $USER:$USER -R "/usr/local/thorium"
+sudo cp "thorium.desktop" "/usr/share/applications/"
+
+echo "Installed Successfully!"
